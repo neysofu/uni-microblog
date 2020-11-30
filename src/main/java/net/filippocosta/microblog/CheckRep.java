f9071c107be5e4430072adc8e6770d8c9805a21f
Filippo Costa <filippocosta.italy@gmail.com>
2020-11-30 16:49:09 +0100

    Finish polishing up `CheckRep`

--- src/main/java/net/filippocosta/microblog/CheckRep.java
+++ src/main/java/net/filippocosta/microblog/CheckRep.java
@@ -7,6 +7,8 @@ interface CheckRep {
     // Nota bene: questo metodo è pensato unicamente per favorire il debugging e
     // la realizzazione della batteria di test.
     //
+    // MODIFIES:
+    //   Nessuna modifica.
     // EFFECTS:
     //   Restituisce `true` se e solo se l'instanza `this` verifica la propria
     //   invariante di rappresentazione, `false` altrimenti.
