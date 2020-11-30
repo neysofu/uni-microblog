1bb4068d0668ba59a249d5de7850b83bbb93ba21
Filippo Costa <filippocosta.italy@gmail.com>
2020-11-30 16:48:23 +0100

    Finish polishing `User` class

--- src/main/java/net/filippocosta/microblog/User.java
+++ src/main/java/net/filippocosta/microblog/User.java
@@ -1,27 +1,22 @@
 package net.filippocosta.microblog;
 
-import java.util.List;
-import java.util.ArrayList;
-
-// Questa classe contiene alcuni metodi statici che assistono nella convalida
-// dei nomi utente all'interno di MicroBlog. Si fornisce inoltre un metodo
-// `suggestOtherUsernames` che propone nomi utente alternativi in caso quello
-// scelto sia già in uso da terzi.
+// OVERVIEW:
+//   Questa classe contiene funzionalità elementari che assistono nella convalida
+//   dei nomi utente all'interno di MicroBlog.
 // 
-// Si sottolinea che -nonostante le funzionalità qui implementate non siano
-// imposte dal documento di specifica- il meccanismo di convalida e di
-// suggerimento nascono per esigenze di emulazione nei confronti di altri
-// social network quali Facebook, Reddit, e Twitter. In particolare, la
-// convalida dei nomi utente pre-iscrizione evita queste situazioni sgradevoli:
+//   Si sottolinea che -nonostante le funzionalità qui implementate non siano
+//   imposte dal documento di specifica- il meccanismo di convalida nasce per
+//   esigenze di emulazione nei confronti di altri social network quali Facebook,
+//   Reddit, e Twitter e in particolare evita queste situazioni sgradevoli:
 // 
-// - Il nome utente è troppo corto (< 3) e risulta poco visibile sullo schermo.
-// - Il nome utente è troppo lungo (> 12) e risulta scomodo da scrivere o
-//   perfino troppo lungo da rientrare nel limite di caratteri di un post.
-// - Il nome utente contiene caratteri accentati, stranieri, spazi o
-//   punteggiatura. Gli unici caratteri ammessi in un nome utente sono:
-//   - Lettere.
-//   - Numeri.
-//   - Trattino basso (l'undescore, ASCII 95).
+//   - Il nome utente è troppo corto (< 3) e risulta poco visibile sullo schermo.
+//   - Il nome utente è troppo lungo (> 12) e risulta scomodo da scrivere o
+//     perfino troppo lungo da rientrare nel limite di caratteri di un post.
+//   - Il nome utente contiene caratteri accentati, stranieri, spazi o
+//     punteggiatura. Gli unici caratteri ammessi in un nome utente sono:
+//     - Lettere.
+//     - Numeri.
+//     - Trattino basso (l'undescore, ASCII 95).
 final public class User {
     // Questa classe non contiene alcuno stato interno e nessun costruttore. Ciò
     // rende superfluo la descrizione di AF e RI.
@@ -66,36 +61,5 @@ final public class User {
     //   Restituisce `true` se e solo se `c` è permesso, `false` altrimenti.
     private static boolean charIsAllowedInUsername(char c) {
         return Character.isLetterOrDigit(c) || c == '_';
-    }
-
-    // Genera un massimo di tre nomi utente simili a quello fornito, evitando
-    // duplicati in `others`.
-    // 
-    // REQUIRES:
-    //   `username != null` e `others != null`.
-    // THROWS:
-    //   `NullPointerException` in caso `username == null` o `others == null`.
-    // MODIFIES:
-    //   Nessuna modifica.
-    // EFFECTS:
-    //   Restituisce una lista di nomi utente generati automaticamente. Questi
-    //   nomi utente sono il più possibile simili a `username`.
-    //   Questo metodo restituisce una lista vuota nel remoto caso in cui nessuno
-    //   dei nomi utente generati sia disponibile.
-    static public List<String> suggestOtherUsernames(String username, List<String> others) {
-        List<String> suggestions = new ArrayList<>();
-        if (!others.contains(username)) {
-            suggestions.add(username);
-            return suggestions;
-        }
-        //int postfixMax = 10;
-        //while (false) {
-        //    for (int i = 0; i < 3; i++) {
-        //        int randomPostfix = 3;
-        //        String suggestion = String.join(username, Integer.toString(randomPostfix));
-        //        suggestions.add(username);
-        //    }
-        //}
-        return suggestions;
     }
 }
